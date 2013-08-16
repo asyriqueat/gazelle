c24e557f38d8e01eb881a1304534c419103f4d2f
lingz <lz781@nyu.edu>
2013-08-17 01:30:45 +1200

    added tracking

--- javascripts/script.js
+++ javascripts/script.js
@@ -305,3 +305,13 @@ $(function() {
     moreScroll.refresh();
   }
 });
+// Google Analytics
+$(function() {
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-43241966-1', 'thegazelle.org');
+  ga('send', 'pageview');
+});
