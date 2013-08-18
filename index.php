33587cde9b52f70e96dce3db59d0cfbc0e2e1032
lingz <lz781@nyu.edu>
2013-08-19 00:35:44 +1200

    margin fixes

--- index.php
+++ index.php
@@ -8,17 +8,20 @@
 
 get_header(); ?>
 
-    <?php
-      $meta_query = new WP_Query(array('posts_per_page' => 10 ));
-      $old_row = 0;
-      while ( $meta_query->have_posts() ) : $meta_query->the_post(); ?>
-          <div class='row gridlock-row'>
-          <div class="article-container col-12" >
-          <?php get_template_part( 'content' ); 
-          // closing the column tag
+    <div class="list list-items">
+
+      <?php
+        $meta_query = new WP_Query(array('posts_per_page' => 10 ));
+        $old_row = 0;
+        while ( $meta_query->have_posts() ) : $meta_query->the_post(); ?>
+            <div class='row gridlock-row'>
+            <div class="article-container col-12" >
+            <?php get_template_part( 'content' ); 
+            // closing the column tag
+            ?>
+            </div>
+            </div>
+      <?php endwhile; 
           ?>
-          </div>
-          </div>
-    <?php endwhile; 
-        ?>
+    </div>
 <?php get_footer(); ?>
