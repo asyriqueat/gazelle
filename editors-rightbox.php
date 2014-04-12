32e92d33d31c20e8404ae73e16f044b91f1b4044
quanvuong <quan.vuong@nyu.edu>
2014-04-13 00:45:18 +0400

    new columnist picture and name

--- editors-rightbox.php
+++ editors-rightbox.php
@@ -11,25 +11,25 @@
       <ul class="columnists-list list-unstyled">
         <li>
           <a href="<?php echo get_site_url().'/issue/author/muhammad-usman/'; ?> ">
-            <img src="http://thegazelle.s3.amazonaws.com/gazelle/2013/10/usman.png" class="img-circle">
-            <!-- <h6>Peace and Conflict</h6> -->
+            <img src="http://thegazelle.s3.amazonaws.com/gazelle/2013/12/Usman.png" class="img-circle">
+            <h6>Peace and Conflict</h6>
           </a>
           <a href="<?php echo get_site_url().'/issue/author/muhammad-usman/'; ?> ">
             <small class="text-muted">Muhammad Usman</small>
           </a>
         </li>
         <li>
-          <a href="<?php echo get_site_url().'/issue/author/olivia-bergenn/'; ?> ">
-            <img src="http://thegazelle.s3.amazonaws.com/gazelle/2013/09/olivia.png" class="img-circle">
+          <a href="<?php echo get_site_url().'/issue/author/massimiliano-valli/'; ?> ">
+            <img src="http://thegazelle.s3.amazonaws.com/gazelle/2013/12/Max.png" class="img-circle">
             <!-- <h6>Days of Our Lives</h6> -->
           </a>
-          <a href="<?php echo get_site_url().'/issue/author/olivia-bergenn/'; ?> ">
-            <small class="text-muted">Olivia Bergen</small>
+          <a href="<?php echo get_site_url().'/issue/author/massimiliano-valli/'; ?> ">
+            <small class="text-muted">Massimiliano Valli</small>
           </a>
         </li>
         <li>
           <a href="<?php echo get_site_url().'/issue/author/joshua-shirley/'; ?> ">
-            <img src="http://thegazelle.s3.amazonaws.com/gazelle/2013/10/josh.png" class="img-circle">
+            <img src="http://thegazelle.s3.amazonaws.com/gazelle/2013/12/Josh.png">
             <!-- <h6>Controversial Things about Australia</h6> -->
           </a>
           <a href="<?php echo get_site_url().'/issue/author/joshua-shirley/'; ?> ">
