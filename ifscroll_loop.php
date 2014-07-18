ff68b6232e8d5a1483211e3881f6bddbe4ef39ea
Asyrique Thevendran <asyrique@gmail.com>
2014-07-18 18:10:14 +0400

    Fixed other syntax errors

--- ifscroll_loop.php
+++ ifscroll_loop.php
@@ -2,7 +2,7 @@
 	<div class="ifscroll-row ifscroll-header-<?php echo strtolower($cat_name); ?>"></div>
 	<div class="ifscroll-issue"><h3><?php echo get_term_by("slug", get_query_var("issue"), "issue")->name; ?></h3></div>
 </div>
-while ( $infinite_posts->have_posts() ) : $infinite_posts->the_post(); ?>
+<?php while ( $infinite_posts->have_posts() ) : $infinite_posts->the_post(); ?>
             <div class='row gridlock-row'>
               <div class="article-container col-12" >
                 <?php get_template_part( 'content' );
