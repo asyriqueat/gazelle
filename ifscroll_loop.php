acc75d56e56e660a7e62cde3143c195cc35f7e39
Asyrique Thevendran <asyrique@gmail.com>
2014-07-18 20:23:45 +0400

    Add identifiers to h3s and cleaned up styling

--- ifscroll_loop.php
+++ ifscroll_loop.php
@@ -1,6 +1,6 @@
 <div class="ifscroll-head">
 	<div class="ifscroll-row ifscroll-header-<?php echo strtolower($category); ?>"></div>
-	<div class="ifscroll-issue"><h3><?php echo get_term_by("slug", $issuenum, "issue")->name; ?></h3></div>
+	<div class="ifscroll-issue-<?php echo strtolower($category); ?>"><h3><?php echo get_term_by("slug", $issuenum, "issue")->name; ?></h3></div>
 </div>
 <?php while ( $infinite_posts->have_posts() ) : $infinite_posts->the_post(); ?>
             <div class='row gridlock-row'>
